251d533e3d1aa5451fe5be6006371cbacc74b721
Erick Mireles <emireles.rosas@gmail.com>
2025-01-30 22:47:16 +0000

    Feat: Add Spinner to Checkout form on pending state

--- src/components/checkout/CheckoutForm.tsx
+++ src/components/checkout/CheckoutForm.tsx
@@ -13,6 +13,7 @@ import {
   SelectItem,
   DatePicker,
   TimeInput,
+  Spinner,
 } from "@heroui/react";
 import { today, getLocalTimeZone, Time } from "@internationalized/date";
 
@@ -138,7 +139,14 @@ export function CheckoutForm({ sucursales }: { sucursales: Sucursal[] }) {
         type="submit"
         className={`${isPending ? "bg-brand bg-opacity-25" : "bg-light bg-opacity-5"} relative mt-4 w-full rounded-sm border border-light border-opacity-25 py-3 text-center leading-none transition duration-75 ease-out ~px-2/4 hover:border-brand hover:border-opacity-50 hover:bg-brand hover:bg-opacity-15 hover:text-brand-2 focus:outline-brand`}
       >
-        {isPending ? <span>Procesando...</span> : <span>Proceder Al Pago</span>}
+        {isPending ? (
+          <div className="mr-4 flex items-center justify-center gap-2">
+            <Spinner size="sm" color="current" />
+            <span>Procesando...</span>
+          </div>
+        ) : (
+          <span>Proceder Al Pago</span>
+        )}
       </button>
     </form>
   );
